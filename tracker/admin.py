9cff81fb11a307c58932bd3f5a00479caa5ce774
John Wrimene <jcordell316@gmail.com>
2026-06-27 09:36:03 -0400

    Update .gitignore, enhance admin model configurations, and add requirements file

--- tracker/admin.py
+++ tracker/admin.py
@@ -5,9 +5,27 @@ from .models import Category, Transaction
 
 @admin.register(Category)
 class CategoryAdmin(admin.ModelAdmin):
-    list_display = ["name", "category_type", "user", "created_at"]
-    list_filter = ["category_type", "created_at"]
-    search_fields = ["name", "user__username"]
+    list_display = [
+        "name",
+        "category_type",
+        "user",
+        "created_at",
+    ]
+    list_filter = [
+        "category_type",
+        "created_at",
+    ]
+    search_fields = [
+        "name",
+        "user__username",
+    ]
+    ordering = [
+        "user__username",
+        "name",
+    ]
+    list_select_related = [
+        "user",
+    ]
 
 
 @admin.register(Transaction)
@@ -20,6 +38,22 @@ class TransactionAdmin(admin.ModelAdmin):
         "user",
         "transaction_date",
     ]
-    list_filter = ["transaction_type", "category", "transaction_date"]
-    search_fields = ["description", "category__name", "user__username"]
+    list_filter = [
+        "transaction_type",
+        "category",
+        "transaction_date",
+    ]
+    search_fields = [
+        "description",
+        "category__name",
+        "user__username",
+    ]
+    ordering = [
+        "-transaction_date",
+        "-created_at",
+    ]
     date_hierarchy = "transaction_date"
+    list_select_related = [
+        "category",
+        "user",
+    ]
